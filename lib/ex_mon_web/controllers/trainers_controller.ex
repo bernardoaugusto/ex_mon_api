d4c4d1ead49efb060419504213298054bfd5ba3b
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-04-13 18:39:35 -0300

    feat: add trainer delete route

--- lib/ex_mon_web/controllers/trainers_controller.ex
+++ lib/ex_mon_web/controllers/trainers_controller.ex
@@ -9,9 +9,19 @@ defmodule ExMonWeb.TrainersController do
     params |> ExMon.create_trainer() |> handle_response(conn)
   end
 
+  def delete(conn, %{"id" => id}) do
+    id |> ExMon.delete_trainer() |> handle_delete(conn)
+  end
+
   defp handle_response({:ok, trainer}, conn) do
     conn |> put_status(:created) |> render("create.json", trainer: trainer)
   end
 
   defp handle_response({:error, _changeset} = error, _conn), do: error
+
+  defp handle_delete({:ok, _trainer}, conn) do
+    conn |> put_status(:no_content) |> text("")
+  end
+
+  defp handle_delete({:error, _reason} = error, _conn), do: error
 end
