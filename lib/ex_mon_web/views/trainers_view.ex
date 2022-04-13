13d0756aa8bba8156d4ad953a981ea8a8d632fdb
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-04-13 19:37:38 -0300

    refactor: refactoring trainers_view

--- lib/ex_mon_web/views/trainers_view.ex
+++ lib/ex_mon_web/views/trainers_view.ex
@@ -13,21 +13,27 @@ defmodule ExMonWeb.TrainersView do
     }
   end
 
-  def render("show.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at}}) do
+  def render("show.json", %{
+        trainer: %Trainer{id: id, name: name, inserted_at: inserted_at, updated_at: updated_at}
+      }) do
     %{
       id: id,
       name: name,
-      inserted_at: inserted_at
+      inserted_at: inserted_at,
+      updated_at: updated_at
     }
   end
 
-  def render("update.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at}}) do
+  def render("update.json", %{
+        trainer: %Trainer{id: id, name: name, inserted_at: inserted_at, updated_at: updated_at}
+      }) do
     %{
       message: "Trainer updated!",
       trainer: %{
         id: id,
         name: name,
-        inserted_at: inserted_at
+        inserted_at: inserted_at,
+        updated_at: updated_at
       }
     }
   end
