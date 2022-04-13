a191404e54a03ba6aea87c7b0b5373d9cf9303da
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2022-04-13 19:57:15 -0300

    feat: add client poke api

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule ExMon.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:tesla, "~> 1.4"}
     ]
   end
 
